a473eb33f10d99b5867dd6b586c12f71db3411bc
Bima alfiansyah <alfiansyahbima@gmail.com>
2021-04-21 23:25:26 +0700

    bima

---
+++ src/Algoritma/JenisKelamin.java
@@ -0,0 +1,16 @@
+package Algoritma;
+
+import java.util.Scanner;
+
+public class JenisKelamin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan Nim : ");
+        String n = scanner.next();
+
+        String nim = n;
+        NIM objNim = new NIM();
+        objNim.setNim(nim);
+        System.out.println("Jenis Kelamin = "+objNim.getJenisKelamin());
+    }
+}
